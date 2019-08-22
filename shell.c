48458e2d9f47d100e333a3b9823cfbebaebf8c32
dhruvsahnan1008 <dhruv.ds.009@gmail.com>
2019-08-22 19:45:52 +0530

    Space separation done.

--- shell.c
+++ shell.c
@@ -1,5 +1,30 @@
 #include <stdio.h>
+#include <string.h>
+#define maxInput 1000
+void takeInput(char *input){
+    printf(">>");
+    fgets(input,maxInput,stdin);
+}
+
+int parseCommand(char* input, char** parsedCommand)
+{
+    takeInput(input);
+    int i;
+    for (i=0; i<maxInput; i++){
+        parsedCommand[i]=strsep(&input, " ");
+        if (parsedCommand[i]==NULL){
+            break;
+        }
+    }
+    return i;
+}
 
 int main(){
-    
+    char input[maxInput];
+    char *parsedCommand[maxInput];
+    int x=parseCommand(input,parsedCommand);
+    // for(int i=0; i<x; i++){
+    //     printf("%s\n",parsedCommand[i]);
+    // }
+    return 0;
 }
